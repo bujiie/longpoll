2411b19024a7255250bd7d85072ad944d6c54095
Brian Ujiie <brian.ujiie@gnarly.local>
2018-09-23 23:33:52 -0700

    Clean up APIs.

--- server.js
+++ server.js
@@ -18,42 +18,28 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 })
 
-app.get('/immediate', (request, response) => {
-  respond(response, 200, {message: 'Yup.', type: 'IMMEDIATE', delay: 0}, 0);
-})
+// immediate, random, specific, range, file
+function defaultResponse(type, message, delay) {
+  return {
+    type,
+    message,
+    delay
+  };
+}
 
-app.get('/random', (request, response) => {
-  const random = randomInRange(1000, 5000);
-  respond(response, 200, {message: 'Hello', type: 'RANDOM', delay: random}, random);
-})
+function errorResponse(type, error, delay) {
+  return {
+    type,
+    error,
+    delay
+  };
+}
 
-app.get('/specific/:delay', (request, response) => {
-  const {delay} = request.params;
-  respond(response, 200, {message: 'Woohoo', type: 'SPECIFIC', delay: delay}, delay);
-})
+function responseFilePath(filename) {
+  return `response/${filename}`;
+}
 
-app.get('/range/:from-:to', (request, response) => {
-  const {from, to} = request.params;
-  const random = randomInRange(from, to);
-  respond(response, 200, {message: 'Yay', type: 'RANGE', delay: random}, random);
-})
-
-app.get('/read/:statusCode/:filename', (request, response) => {
-  const {statusCode, filename} = request.params;
-  try {
-    const stats = fs.statSync(`response/${filename}.json`);
-    if (stats.isFile()) {
-      const body = fs.readFileSync(`response/${filename}.json`, {encoding: 'utf-8'});
-      respond(response, statusCode, body, 0);
-    } else {
-      throw new Error(`${filename}.json cannot be found.`);
-    }
-  } catch (error) {
-    respond(response, 400, {'error': `Could not find ${filename}.json.`}, 0);
-  }
-})
-
-function respond(response, statusCode, body = null, delay) {
+function respond(response, statusCode, body = null, delay = 0) {
   setTimeout(() => {
     response
       .status(statusCode)
@@ -70,7 +56,7 @@ function toNumber(value) {
   return isNumber(value) ? parseInt(value) : null;
 }
   
-function randomInRange(min, max) {
+function randomIntWithinRange(min, max) {
   const minimum = toNumber(min);
   const maximum = toNumber(max);
   if (minimum > maximum) {
@@ -78,84 +64,119 @@ function randomInRange(min, max) {
   }
   return Math.floor(Math.random() * (maximum - minimum)) + minimum;
 }
+
+const Type = {
+  IMMEDIATE: 'IMMEDIATE',
+  RANDOM: 'RANDOM',
+  SPECIFIC: 'SPECIFIC',
+  RANGE: 'RANGE',
+  FILE: 'FILE'
+};
+
+const StatusCode = {
+  OK: 200,
+  CREATE_OK: 201,
+  BAD: 400,
+  NOT_AUTHORIZED: 401,
+  FORBIDDEN: 403
+};
+
+const Delay = {
+  ZERO: 0,
+  ONE_SECOND: 1000,
+  FIVE_SECOND: 5000
+}
+
+
+app.get('/immediate', (request, response) => {
+  respond(
+    response, 
+    StatusCode.OK, 
+    defaultResponse(Type.IMMEDIATE, 'Immediate success!', Delay.ZERO), 
+    Delay.ZERO
+  );
+})
+
+app.get('/random', (request, response) => {
+  const random = randomIntWithinRange(Delay.ONE_SECOND, Delay.FIVE_SECOND);
+  respond(
+    response, 
+    StatusCode.OK,
+    defaultResponse(Type.RANDOM, 'Random success!', random), 
+    random
+  );
+})
+
+app.get('/specific/:delay', (request, response) => {
+  const {delay} = request.params;
+  respond(
+    response, 
+    StatusCode.OK,
+    defaultResponse(Type.SPECIFIC, 'Specific success!', delay), 
+    delay
+  );
+})
+
+app.get('/range/:from-:to', (request, response) => {
+  const {from, to} = request.params;
+  const random = randomIntWithinRange(from, to);
+  respond(
+    response, 
+    StatusCode.OK,
+    defaultResponse(Type.RANGE, 'Random success!', random), 
+    random
+  );
+})
+
+app.get('/read/:filename/status/:statusCode/delay/:delay', (request, response) => {
+  const {filename, statusCode, delay} = request.params;
+  const fullFilename = `${filename}.json`;
+  const filePath = responseFilePath(fullFilename);
+  try {
+    const stats = fs.statSync(filePath);
+    if (stats.isFile()) {
+      const body = fs.readFileSync(filePath, {encoding: 'utf-8'});
+      respond(response, statusCode, body, delay);
+    } else {
+      throw new Error(`${fullFilename} cannot be found.`);
+    }
+  } catch (error) {
+    respond(
+      response, 
+      StatusCode.BAD,
+      errorResponse(Type.SPECIFIC, `Could not find ${fullFilename}.`, delay),
+      delay
+    );
+  }
+})
+
+app.get('/read/:filename/status/:statusCode', (request, response) => {
+  const {filename, statusCode} = request.params;
+  const fullFilename = `${filename}.json`;
+  const filePath = responseFilePath(fullFilename);
+  try {
+    const stats = fs.statSync(filePath);
+    if (stats.isFile()) {
+      const body = fs.readFileSync(filePath, {encoding: 'utf-8'});
+      respond(response, statusCode, body, Delay.ZERO);
+    } else {
+      throw new Error(`${filename}.json cannot be found.`);
+    }
+  } catch (error) {
+    respond(
+      response, 
+      StatusCode.BAD, 
+      errorResponse(Type.IMMEDIATE, `Could not find ${fullFilename}.`, Delay.ZERO),
+      Delay.ZERO
+    );
+  }
+})
+
+
   
 
 
 app.get('/testing', (request, response) => {
   response.status(200).type('application/json').send({});
 })
-
-// let q = [];
-
-// function enqueue(queue, value) {
-//   queue.push(value);
-// }
-
-// function dequeue(queue) {
-//   queue.shift();
-// }
-
-// function peek(queue) {
-//   return queue[queue.length - 1];
-// }
-
-// function isFull(queue) {
-//   return false;
-// }
-
-// function isEmpty(queue) {
-//   return queue.length == 0;
-// }
-
-// app.get('/queue', (request, response) => {
-//   const startTime = new Date();
-//   const id = setInterval(() => {
-//     if (!isEmpty(q)) {
-//       const stopTime = new Date();
-//       respond(response, stopTime - startTime);
-//       dequeue(q);
-//       clearInterval(id);
-//     }
-//   }, 100);
-// })
-
-// app.get('/queue/push', (request, response) => {
-//   enqueue(q, "data");
-//   respond(response, 0);
-// })
-
-
-
-
-
-
-
-
-// function send(response, statusCode, body, delay = 0) {
-//   setTimeout(() => {
-//     response
-//       .status(statusCode)
-//       .header({'Access-Control-Allow-Origin': '*'})
-//       .type('application/json')
-//       .send(JSON.stringify(body));
-//   }, delay);
-// }
-
-// function respond(response, waitMs) {
-//   setTimeout(() => {
-//     response
-//       .status(200)
-//       .header({'Access-Control-Allow-Origin': '*'})
-//       .type('application/json')
-//       .send(JSON.stringify({timestamp: new Date(), waitMs: waitMs}));
-//   }, waitMs)
-// }
-
-// function error(response, message) {
-//   response
-//       .status(400)
-//       .header({'Access-Control-Allow-Origin': '*'})
-//       .type('application/json')
-//       .send(JSON.stringify({"error": message}));
-// }
 
